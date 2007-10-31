b9b4b678b4e825e269e029f22412ad3fe750a3ee
arthurthiago <arthurthiago@4246d16a-cc3d-0410-94fa-b9fcf0a18e93>
2007-10-31 22:43:26 +0000

    * huffman.c - Comecei a implementar a árvore.

--- trunk/Trabalhos/OA/Huffman/huffman.c
+++ trunk/Trabalhos/OA/Huffman/huffman.c
@@ -7,40 +7,44 @@
 typedef struct {
     char caractere;
     int frequencia;
-} lista_t;
+    struct no_arv *esq;
+    struct no_arv *dir;
+} no_arv;
 
-lista_t *ordenarLista(lista_t *lista, int tamanho);
-lista_t *construirLista(FILE *arq, int *tamanho);
-void mostrarLista(lista_t *lista, int tamanho);
+no_arv *ordenarLista(no_arv *no, int tamanho);
+no_arv *construirVetor(FILE *arq, int *tamanho);
+void mostrarLista(no_arv *no, int tamanho);
 
 int main(){
     FILE *arq;
-    lista_t *lista;
+    no_arv *vetor;
     int tamanho = 0;
 
-    lista = construirLista(arq, &tamanho);
-    mostrarLista(lista, tamanho);
+    no = construirLista(arq, &tamanho);
+    mostrarLista(vetor, tamanho);
 
     return 0;
 }
 
-lista_t *construirLista(FILE *arq, int *tamanho) {
+no_arv *construirVetor(FILE *arq, int *tamanho) {
     int achou = 0, i = 0;
     char ch;
-    lista_t *lista, *pont;
+    no_arv *no, *pont;
 
     /* Abre o arquivo. */
     arq = fopen(NOME_ARQ, "r");
 
     *tamanho = 1;
-    lista = (lista_t *) calloc(1, sizeof(lista_t));
-    pont = lista;
+    no = (no_arv *) calloc(1, sizeof(no_arv));
+    pont = no;
     ch = fgetc(arq);
     pont->caractere = ch;
     pont->frequencia = 1;
+    pont->esq = NULL;
+    pont->dir = NULL;
     while ((ch = fgetc(arq)) != EOF) {
 	for (i = 0; i <= *tamanho - 1; i++) {
-	    pont = &lista[i];
+	    pont = &no[i];
 	    if (pont->caractere == ch) {
 		pont->frequencia += 1;
 		achou = 1;
@@ -49,29 +53,33 @@ lista_t *construirLista(FILE *arq, int *tamanho) {
 	}
 	if (achou == 0) {
 	    *tamanho += 1;
-	    lista = (lista_t *) realloc(lista, *tamanho * sizeof(lista_t));
-	    pont = &lista[*tamanho - 1];
+	    no = (no_arv *) realloc(no, *tamanho * sizeof(no_arv));
+	    pont = &no[*tamanho - 1];
 	    pont->caractere = ch;
 	    pont->frequencia = 1;
+	    pont->esq = NULL;
+	    pont->dir = NULL;
 	} else {
 	    achou = 0;
 	}
 
     }
 
-    lista = ordenarLista(lista, *tamanho);
-
-    return lista;
+    return qsort(no, *tamanho, sizeof(no_arv), compararNos);
 }
 
-lista_t *ordenarLista(lista_t *lista, int tamanho) {
+int compararNos(const no_arv* a, const no_arv* b) {
+    return a->frequencia - b->frequencia;
+}
+
+no_arv *ordenarLista(no_arv *no, int tamanho) {
     int i = 0, j = 0;
-    lista_t temp, *pont1, *pont2;
+    no_arv temp, *pont1, *pont2;
 
     for (i = 0; i <= tamanho - 1; i++) {
 	for (j = 0; j <= tamanho - 2; j++) {
-	    pont1 = &lista[j];
-	    pont2 = &lista[j + 1];
+	    pont1 = &no[j];
+	    pont2 = &no[j + 1];
 	    if (pont1->frequencia > pont2->frequencia) {
 		temp.caractere = pont1->caractere;
 		temp.frequencia = pont1->frequencia;
@@ -82,13 +90,29 @@ lista_t *ordenarLista(lista_t *lista, int tamanho) {
 	    }
 	}
     }
-    
-    return lista;
+
+    return no;
 }
 
-void mostrarLista(lista_t *lista, int tamanho) {
+void mostrarVetor(no_arv *vetor, int tamanho) {
     int i = 0;
+    printf("---Tabela de Frequências---\n");
     for (i = 0; i <= tamanho - 1; i++) {
-	printf("%c : %d \n",lista[i].caractere, lista[i].frequencia);
+	printf("%c : %d \n",vetor[i].caractere, vetor[i].frequencia);
     }
+    printf("---------------------------\n");
+}
+
+void construirArvore(no_arv *vetor, int tamanho) {
+    no_arv *pont;
+    int i = 0;
+
+    while () {
+	pont = vetor[i];
+	while () {
+	    
+	}
+	i++;
+    }
+
 }
