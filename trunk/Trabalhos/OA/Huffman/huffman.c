b3b6f68061bc9ecd9506c3602c0287339ea033e7
arthurthiago <arthurthiago@4246d16a-cc3d-0410-94fa-b9fcf0a18e93>
2007-11-06 21:52:57 +0000

    - Estou ainda implementando a parte de criar a árvore utilizando listas. Está dando uns erros que não estou conseguindo identificar.

--- trunk/Trabalhos/OA/Huffman/huffman.c
+++ trunk/Trabalhos/OA/Huffman/huffman.c
@@ -4,11 +4,14 @@
 
 #define NOME_ARQ "arquivo.txt"
 #define TAM_MAX 32
+#define CAR_ESP 127
 
 /** Estrutura do nó da árvore. */
 struct no_arv {
     char caractere;
     int frequencia;
+    int codigo;
+    int tam_cod;
     struct no_arv *ant;
     struct no_arv *prox;
     struct no_arv *esq;
@@ -19,10 +22,11 @@ struct no_arv {
 typedef struct no_arv no_arv;
 
 no_arv *construirLista(FILE *arq, int *tamanho);
-no_arv *ordenarLista(no_arv *inicio, int tamanho);
+void ordenarLista(no_arv *pinicio, int tamanho);
 void mostrarLista(no_arv *pinicio);
-no_arv *construirArvore(no_arv *pinicio);
+void construirArvore(no_arv *pinicio);
 void mostrarArvore(no_arv *arv);
+void gerarCodigos(no_arv *arv, int profundidade, int codigo);
 
 
 int main() {
@@ -32,8 +36,9 @@ int main() {
 
     listaArvore = construirLista(arq, &tamanho);
     mostrarLista(listaArvore);
-    /*listaArvore = construirArvore(listaArvore);
-    mostrarArvore(listaArvore);*/
+    construirArvore(listaArvore);
+    mostrarArvore(listaArvore);
+    gerarCodigos(listaArvore, 0, 0);
 
     return 0;
 }
@@ -58,7 +63,6 @@ no_arv *construirLista(FILE *arq, int *tamanho) {
     paux->esq = NULL;
     paux->dir = NULL;
     *tamanho = 1;
-
     /* Adiciona os outros elementos à lista. */
     while ((ch = fgetc(arq)) != EOF) {
 	paux = pinicio;
@@ -88,99 +92,115 @@ no_arv *construirLista(FILE *arq, int *tamanho) {
 	}
 
     }
-
-    return ordenarLista(pinicio, *tamanho);
-}
-
-no_arv *ordenarLista(no_arv *inicio, int tamanho) {
-    int i = 0, j = 0;
-    no_arv *paux, *ptroca;
-    for (i = 0; i <= tamanho - 1; i++) {
-	printf("%d\n", tamanho);
-	printf("%d\n", i);
-	paux = inicio;
-	for (j = 0; j <= tamanho - 2; i++) {
-	    printf("foi");
-	    ptroca = paux->prox;
-	    if ((paux->frequencia > ptroca->frequencia) || ((paux->frequencia == ptroca->frequencia) && (paux->caractere > ptroca->caractere))) {
-		paux->prox = ptroca->prox;
-		ptroca->ant = paux->ant;
-		ptroca->prox->ant = paux;
-		ptroca->prox = paux;
-		paux->ant->prox = ptroca;
-		paux->ant = ptroca;
-	    }
-	    paux = paux->prox;
-	}
-    }
-    return inicio;
-}
-
-void mostrarLista(no_arv *pinicio) {
-    no_arv *paux = pinicio;
-    printf("\n\n---Tabela de Frequências---\n");
-    while (paux != NULL) {
-	printf("|%c : %d| -> ",paux->caractere, paux->frequencia);
-	paux = paux->prox;
-    }
-    printf("NULL\n");
-    printf("---------------------------\n\n\n");
-}
-
-no_arv *construirArvore(no_arv *pinicio) {
-    no_arv *paux, *paux2, *pnovo;
-
-    while (paux != NULL) {
-	paux = pinicio;
-	paux2 = paux->prox;
-	pnovo = (no_arv *) calloc (1, sizeof(no_arv));
-	
-	pnovo->esq = paux;
-	paux->prox = NULL;
-	pnovo->dir = paux2;
-	paux2->ant = NULL;
-	paux2->prox->ant = NULL;
-	paux2->prox = NULL;
-	
-	pnovo->frequencia = paux->frequencia + paux2->frequencia;
-	
-	/* Ordena a lista. */
-    }
-
+    //ordenarLista(pinicio, *tamanho);
     return pinicio;
 }
 
-void mostrarArvore(no_arv *arv) {
-    if (arv != NULL)
-    {
-	printf("%c : %d\n",arv->caractere, arv->frequencia);
-	mostrarArvore(arv->esq);
-	mostrarArvore(arv->dir);
-    } else {
-	printf("NULL\n");
-    }
-}
-/*
-el_cod *gerarListaCodigos(no_arv *arv, int tamanho) {
-    el_cod *cod;
-    int indice;
-    char codigo[TAM_MAX];
-
-    cod = calloc(tamanho, sizeof(el_cod));
-    preencherCodigos(cod, arv, &indice, codigo);
-    return cod;
-}
-
-void preencherCodigos(el_cod *cod, no_arv *arv, int *indice, char codigo[TAM_MAX]) {
-    if ((arv != NULL &&) {
-	 if (arv->caractere != CAR_ESP)) {
-	    cod[*indice].codigo = codigo;
-	    *indice += 1;
-	    return;
-	 } else {
-	    preencherCodigos(cod, arv->esq, indice, strcat(codigo, "0"));
-	    preencherCodigos(cod, arv->dir, indice, strcat(codigo, "1"));
+void ordenarLista(no_arv *pinicio, int tamanho) {
+    int i, j;
+    no_arv *paux, *ptroca;
+    for (i = 0; i <= tamanho - 1; i++) {
+	paux = pinicio;
+	if (pinicio->frequencia > paux->prox->frequencia) {
+	    ptroca = paux->prox;
+	    paux->prox = ptroca->prox;
+	    paux->ant = ptroca;
+	    ptroca->prox->ant = paux;
+	    ptroca->prox = paux;
+	    ptroca->ant = NULL;
+	    pinicio = ptroca;
+	} else {
+	    paux = paux->prox;
+	}
+	for (j = 0; j <= tamanho - 2; j++) {
+	    if (paux->frequencia > paux->prox->frequencia) {
+		ptroca = paux->prox;
+		paux->prox = ptroca->prox;
+		paux->ant->prox = ptroca;
+		ptroca->ant = paux->ant;
+		ptroca->prox->ant = paux;
+		paux->ant = ptroca;
+		ptroca->prox = paux;
+	    } else {
+		paux = paux->prox;
+	    }
 	}
     }
-}*/
+}
 
+void mostrarLista(no_arv *pinicio) {
+    no_arv *paux;
+
+    paux = pinicio;
+    printf("\n\n---Lista de Frequências (PROX)---\n");
+    printf("NULL = ");
+    printf("|%c : %d| = ",paux->caractere, paux->frequencia);
+    while (paux->prox != NULL) {
+	paux = paux->prox;
+	printf("|%c : %d| = ",paux->caractere, paux->frequencia);
+    }
+    printf("NULL\n");
+    printf("----------------------------------\n");
+    printf("\n---Lista de Frequências (ANT)---\n");
+    printf("NULL = ");
+    while (paux != NULL) {
+	printf("|%c : %d| = ",paux->caractere, paux->frequencia);
+	paux = paux->ant;
+    }
+    printf("NULL\n");
+    printf("----------------------------------\n\n");
+}
+
+void construirArvore(no_arv *pinicio) {
+    no_arv *paux, *pnovo;
+
+    paux = pinicio;
+    while (pinicio->prox != NULL) {
+	paux = pinicio->prox;
+	pnovo = (no_arv *) calloc (1, sizeof(no_arv));
+	
+	pnovo->esq = pinicio;
+	pnovo->dir = paux;
+	if (paux->prox != NULL) {
+	    pnovo->prox = paux->prox;
+	    paux->prox->ant = pnovo;
+	}
+	pnovo->ant = NULL;
+	pinicio->prox = NULL;
+	paux->ant = NULL;
+	paux->prox = NULL;
+	pnovo->frequencia = pinicio->frequencia + paux->frequencia;
+	pnovo->caractere = CAR_ESP;
+	printf("%c", pnovo->caractere);
+	printf("%c", pinicio->caractere);
+	printf("%c", paux->caractere);
+
+	pinicio = pnovo;
+    }
+}
+
+void mostrarArvore(no_arv *arv) {
+    if (arv != NULL) {
+	printf("\n");
+	printf("|%c : %d| ",arv->caractere, arv->frequencia);
+	mostrarArvore(arv->esq);
+	mostrarArvore(arv->dir);
+	printf("\n");
+    } else {
+	printf("NULL ");
+    }
+}
+
+void gerarCodigos(no_arv *arv, int profundidade, int codigo) {
+    if (arv->caractere != CAR_ESP) {
+	arv->codigo = codigo;
+	arv->tam_cod = profundidade;
+	return;
+    } else {
+	codigo <<= 1;
+	profundidade++;
+	gerarCodigos(arv->esq, profundidade, codigo);
+	codigo |= 1;
+	gerarCodigos(arv->dir, profundidade, codigo);
+    }
+}
