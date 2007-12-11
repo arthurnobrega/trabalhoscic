664625d0e7b32159e3e68259ee4f11ee6eb0646d
arthurthiago <arthurthiago@4246d16a-cc3d-0410-94fa-b9fcf0a18e93>
2007-12-11 17:05:06 +0000

    atualizacoes

--- trunk/Trabalhos/OA/Visualizacao/Visualizacao.c
+++ trunk/Trabalhos/OA/Visualizacao/Visualizacao.c
@@ -21,7 +21,7 @@ void mostrarMenu() {
     printf("    - DESC arg1 arg2\n");
     printf("    - RELAT arg \n");
     printf("    - SAIR\n\n");
-    printf("Para obter ajuda sobre os comandos digite COMANDO --help\n\n");
+    printf("Digite o comando seguido dos argumentos:\n\n");
 
     while (strcmp(comando, "SAIR")) {
 	printf("> ");
@@ -33,9 +33,10 @@ void mostrarMenu() {
 	    int tamanho = 0;
 
 	    scanf("%s", comando);
-	    // Testar se o arquivo existe
 	    arv = c_gerarArvoreHuffman(comando, &tamanho);
 	    mostrarTabelaHuffman(arv, tamanho);
+	    // Limpa a memória.
+	    free(arv);
 	} else if (!strcmp(comando, "HUFF")) {
 	    char arqEntrada[TAM_MAX], arqSaida[TAM_MAX];
 	    no_arv *arv;
@@ -44,7 +45,7 @@ void mostrarMenu() {
 	    scanf("%s", arqEntrada);
 	    scanf("%s", arqSaida);
 	    c_compactarHuffman(arqEntrada, arqSaida, arv, &tamanho);
-	    printf("Arquivo compactado e salvo com sucesso!\n");
+	    printf("O Arquivo \"%s\" foi compactado como \"%s\" utilizando Huffman!\n", arqEntrada, arqSaida);
 	} else if (!strcmp(comando, "DIC_LZ")) {
 	    char arqEntrada[TAM_MAX];
 	    scanf("%s", arqEntrada);
@@ -57,11 +58,12 @@ void mostrarMenu() {
 	} else if (!strcmp(comando, "LZ")) {
 	    char arqEntrada[TAM_MAX], arqSaida[TAM_MAX];
 	    tab* tabelaLZ = NULL;
-	    
+
 	    scanf("%s", arqEntrada);
 	    scanf("%s", arqSaida);
 	    tabelaLZ = c_criarTabelaLempelZiv(arqEntrada);
 	    c_compactarLempelZiv(tabelaLZ, arqSaida);
+	    printf("O Arquivo \"%s\" foi compactado como \"%s\" utilizando Lempel-Ziv!\n", arqEntrada, arqSaida);
 	    // Limpa a memória.
 	    free(tabelaLZ);
 	} else if (!strcmp(comando, "DESC")) {
@@ -70,7 +72,7 @@ void mostrarMenu() {
 	    scanf("%s", arqEntrada);
 	    scanf("%s", arqSaida);
 	    c_descomprimirArquivo(arqEntrada, arqSaida);
-	    printf("Arquivo descompactado com sucesso!\n");
+	    printf("O Arquivo \"%s\" foi descompactado como \"%s\"!\n", arqEntrada, arqSaida);
 	} else if (!strcmp(comando, "RELAT")) {
 	    char arqEntrada[TAM_MAX];
 	    int *nroBits;
@@ -78,6 +80,8 @@ void mostrarMenu() {
 	    scanf("%s", arqEntrada);
 	    nroBits = c_gerarRelatorio(arqEntrada);
 	    mostrarRelatorio(nroBits, arqEntrada);
+	    // Limpa a memória.
+	    free(nroBits);
 	} else if (strcmp(comando, "SAIR")){
 	    printf("Comando \"%s\" não encontrado.\n", comando);
 	}
@@ -90,59 +94,64 @@ void mostrarTabelaLempelZiv(tab *tabelaLempelZiv) {
     int conv = 0;
 
     paux = tabelaLempelZiv;
-    printf("INDICE       ANTERIOR        LETRA\n");
-    printf("   0            -              -\n");
+    printf("\n--------Dicionário de Lempel-Ziv--------\n");
+    printf("   Índice       Anterior        Letra\n");
+    printf("      0            -              -\n");
     while (paux->prox != NULL) {
-	printf("%4d",paux->indice);
+	printf("%7d",paux->indice);
 	printf("%13d",paux->indiceAnterior);
 	printf("%15d\n",paux->letraRaiz);
 	paux = paux->prox;
     }
     conv = paux->letraRaiz;
     if (conv != 255) {
-	printf("%4d",paux->indice);
+	printf("%7d",paux->indice);
 	printf("%13d",paux->indiceAnterior);
 	printf("%15d\n",paux->letraRaiz);
     }
+    printf("----------------------------------------\n\n");
 }
 
 void mostrarRelatorio(int *nroBits, char *narq) {
     float contaHuff, contaLZ;
 
-    printf("--------------------------------\n");
-    printf("Nome do Arquivo:      ");
+    printf("\n------------Relatório-------------\n");
+    printf("Nome do Arquivo:        ");
     puts(narq);
     printf("\n");
-    printf("Tamanho Original:     ");
+    printf("Tamanho Original:       ");
     printf("%d bits\n", nroBits[0]);
-    printf("Tamanho HUFF:         ");
+    printf("Compactando com:\n");
+    printf("- Huffman\n");
+    printf("   Tamanho:             ");
     printf("%d bits\n", nroBits[1]);
-    printf("COMP HUFF:            ");
+    printf("   Taxa de Compressao:  ");
     contaHuff = (double) (100 * (1 - ((double)nroBits[1]/(double)nroBits[0])));
     printf("%0.1f \%\n", contaHuff);
-    printf("Tamanho LZ:           ");
+    printf("- Lempel-Ziv\n");
+    printf("   Tamanho:             ");
     printf("%d bits\n", nroBits[2]);
-    printf("COMP LZ:              ");
+    printf("   Taxa de Compressao:  ");
     contaLZ = (double) (100 * (1 - ((double)nroBits[2]/(double)nroBits[0])));
-    printf("%0.1f \%\n", contaLZ);
-    printf("\nMelhor Algorítmo:     ");
+    printf("%0.1f \%\n\n", contaLZ);
+    printf("Melhor Algorítmo:       ");
     (contaHuff > contaLZ) ? printf("Huffman\n") : printf("Lempel-Ziv\n");
-    printf("--------------------------------\n");
+    printf("----------------------------------\n\n");
 }
 
 /* Mostra a tabela de Huffman na tela. */
 void mostrarTabelaHuffman(no_arv *arv, int tamanho) {
-    printf("--------Tabela de Códigos---------\n");
-    printf("%d\n", tamanho);
+    printf("\n--------Tabela de Códigos de Huffman---------\n");
+    printf("%5d\n", tamanho);
     mostrarCodigosHuffman(arv);
-    printf("----------------------------------\n");
+    printf("--------------------------------------------\n\n");
 }
 
 /* Mostra os caracteres e seus respectivos códigos da tabela de Huffman. */
 void mostrarCodigosHuffman(no_arv *arv) {
     if (arv->esq == NULL) {
 	int profundidade, i;
-	printf("%d ",arv->caractere);
+	printf("%5d ",arv->caractere);
 	profundidade = arv->profundidade;
 	for (i = 0; i <= profundidade - 1; i++) {
 	    putc(arv->codigo[i], stdout);
